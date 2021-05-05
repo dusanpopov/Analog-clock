2365e98b79b003314d79e2fc98117955279cbfe7
Dušan Popov <aristocut90@gmail.com>
2021-05-05 16:31:46 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const hourHand = document.querySelector('[data-hour-hand]');
 const minuteHand = document.querySelector('[data-minute-hand]');
 const secondHand = document.querySelector('[data-second-hand]');
 
-const setRotation = (element, rotationRation) => {
+const setRotation = (element, rotationRatio) => {
   element.style.setProperty('--rotation', rotationRatio * 360)
 }
 
